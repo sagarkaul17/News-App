ca566599301c144df52c93c67d92553fce0560f5
Sagar Kaul <kaul.sagar@indiamart.com>
2021-11-10 10:22:29 +0530

    Version 2 done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import './App.css';
-
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <Router>
         <NavBar/>
-        <News pageSize={9}/>
-      </div>
+        <Switch>
+          <Route exact path="/">
+            <News pageSize={9}/>
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+        </Switch>
+      </Router>
     )
   }
 }
--- src/NewsItem.js
+++ src/NewsItem.js
@@ -5,11 +5,11 @@ export class NewsItem extends Component {
         let {title,description,imageUrl,newsUrl} = this.props;
         return (
             <div className="card" >
-                <img src={imageUrl} className="card-img-top" alt="..."/>
+                <img src={imageUrl} style={{height:250}}className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
-                    <a href={newsUrl} className="btn btn-sm btn-primary">Read more</a>
+                    <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
                 </div>
             </div>
         )
